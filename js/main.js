215127ca11b7180ce21fa1a105b8c23bc3f092e9
tonyap92 <114396404+tonyap92@users.noreply.github.com>
2025-01-04 16:39:57 +0300

    Added functionality for navigating to the menu.

--- js/main.js
+++ js/main.js
@@ -5,11 +5,50 @@ const linesElement = document.querySelector(".hello-header__lines");
 const menuContainer = document.querySelector(".hello-menu");
 const bodyContainer = document.querySelector("body");
 
+const startContainer = document.querySelector(".hello-start");
+const buttonBeginningElement = document.querySelector(".btn--beginning");
+const buttonStandingsElement = document.querySelector(".btn--standings");
+const standingsContainer = document.querySelector(".hello-standings");
+
+const windows = [startContainer, standingsContainer];
+
 // FUNCTIONS
+// Функция переключения видимости меню
 const toggleMenuVisibility = () => {
   menuContainer.classList.toggle("hidden");
   bodyContainer.classList.toggle("active");
 };
 
+// Функция перенаправления на стартовую страницу из меню
+const showStartsWindow = () => {
+  redirectToIndex();
+  toggleMenuVisibility();
+  if (startContainer) {
+    startContainer.classList.add("hidden");
+  }
+};
+
+// Функция показа окна с турнирной таблицей
+const showStandingsWindow = () => {
+  toggleMenuVisibility();
+
+  windows.forEach((window) => {
+    if (window && window !== standingsContainer) {
+      window.classList.add("hidden");
+    }
+  });
+
+  if (standingsContainer) {
+    standingsContainer.classList.remove("hidden");
+  }
+};
+
+// Функция перенаправления на стартовую страницу с кнопок
+const redirectToIndex = () => {
+  location.href = "index.html";
+};
+
 // LISTENERS
 linesElement.addEventListener("click", toggleMenuVisibility);
+buttonBeginningElement.addEventListener("click", showStartsWindow);
+buttonStandingsElement.addEventListener("click", showStandingsWindow);
