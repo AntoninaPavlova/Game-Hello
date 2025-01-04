725380f5f1c1118915cb7b2e424de1b1c97f900c
tonyap92 <114396404+tonyap92@users.noreply.github.com>
2025-01-04 16:14:30 +0300

    I have implemented the menu functionality.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 "use strict";
 
 // VARIABLES
+const linesElement = document.querySelector(".hello-header__lines");
+const menuContainer = document.querySelector(".hello-menu");
+const bodyContainer = document.querySelector("body");
 
 // FUNCTIONS
+const toggleMenuVisibility = () => {
+  menuContainer.classList.toggle("hidden");
+  bodyContainer.classList.toggle("active");
+};
 
 // LISTENERS
+linesElement.addEventListener("click", toggleMenuVisibility);
